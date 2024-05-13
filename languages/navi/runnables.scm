23994078b3503aa767e8f98b76b02bd0116b5a0b
Jason Lee <huacnlee@gmail.com>
2024-05-13 10:37:26 +0800

    Add `navi run` task to `fn main`.

--- languages/navi/runnables.scm
+++ languages/navi/runnables.scm
@@ -1,3 +1,17 @@
 (
     (test_item name: (_) @run)
 ) @navi-test
+
+(
+    (bench_item name: (_) @run)
+) @navi-bench
+
+(
+    (source_file) @run
+) @navi-file
+
+(
+    (function_item
+        name: (_) @_name
+        (#match? @_name "main")) @run
+) @navi-run
